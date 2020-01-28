345ca847f4af555f7bd516b71ca6f6818d742302
JackHilon <markandmark1111@gmail.com>
2020-01-28 01:21:13 -0800

    Add project files.

---
+++ LexiconLMS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LexiconLMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
