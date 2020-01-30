10ea5c1c31d7116ebcdd0c076324dba9cd7bc7c1
Radhika Jayaraman <radhika_jram@outlook.com>
2020-01-30 09:48:55 +0100

    Check for Edit  view screen not to update start date.

--- LexiconLMS/Controllers/CoursesController.cs
+++ LexiconLMS/Controllers/CoursesController.cs
@@ -88,7 +88,7 @@ namespace LexiconLMS.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CourseId,CourseName,CourseDescription,StartDate")] Course course)
+        public async Task<IActionResult> Edit(int id, [Bind("CourseId,CourseName,CourseDescription")] Course course)
         {
             if (id != course.CourseId)
             {
@@ -99,7 +99,17 @@ namespace LexiconLMS.Controllers
             {
                 try
                 {
-                    _context.Update(course);
+                    var newUpdatedDataCourse = await _context.Courses.FindAsync(id);
+
+                    if(newUpdatedDataCourse == null)
+                    {
+                        return NotFound();
+                    }
+
+                    newUpdatedDataCourse.CourseId = course.CourseId;
+                    newUpdatedDataCourse.CourseName = course.CourseName;
+                    newUpdatedDataCourse.CourseDescription = course.CourseDescription;
+                  //  _context.Update(course);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
---
+++ LexiconLMS/Data/Migrations/20200130075829_checkdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LexiconLMS.Data.Migrations
+{
+    public partial class checkdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
