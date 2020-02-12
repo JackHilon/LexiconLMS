9f09140ce120b1a61009f923afbfb0ed98d6562a
Ninib <ninib.hadrouli@hotmail.com>
2020-02-12 15:56:04 +0100

    Add Validation to Module, ModuleActivitiy ,Course

--- LexiconLMS/Migrations/20200212144820_ModuleValidation.Designer.cs
+++ LexiconLMS/Migrations/20200212144820_ModuleValidation.Designer.cs
@@ -4,14 +4,16 @@ using LexiconLMS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LexiconLMS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200212144820_ModuleValidation")]
+    partial class ModuleValidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- LexiconLMS/Models/Module.cs
+++ LexiconLMS/Models/Module.cs
@@ -11,7 +11,9 @@ namespace LexiconLMS.Models
        // private const object DataType;
 
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
